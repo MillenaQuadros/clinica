f6b9a3479383225faacefbdf42682242884aa469
MillenaQuadros <lauraquadros775@gmail.com>
2024-11-15 20:15:39 -0300

    Comit

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php 
+        $pdo = new PDO ("mysql:dbname=clinicaMed;host=localhost:3306","root","nova_senha");
+
+
+        /*if ($pdo){
+            echo "Banco conectado";
+        }*/
+
+
+        
+    ?>
